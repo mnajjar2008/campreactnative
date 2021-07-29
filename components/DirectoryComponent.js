59c98fb81322ff320473b45174ab6e35f7863cb5
Mo <None>
2021-07-29 14:26:17 -0700

    Week 3 Workshop Assignment

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, FlatList, Text } from 'react-native';
-import { ListItem } from 'react-native-elements';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
