244fdca7878d9606a4a0ffc082424f9ef53f7f3b
Mo <None>
2021-07-30 09:16:33 -0700

    Exercise: Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
     constructor(props) {
@@ -21,8 +22,8 @@ class Reservation extends Component {
 
     handleReservation() {
         Alert.alert(
-            'Delete Favorite?',
-            'Number of Campers: ' + this.state.campers + "\n"+"HikeIn? " +this.state.hikeIn+ "\n"+"Date: "+ this.state.date,
+            'Begin search',
+            'Number of Campers: ' + this.state.campers + '\n' + 'HikeIn? ' + this.state.hikeIn + '\n' + 'Date: ' + this.state.date,
             [
                 {
                     text: 'Cancel',
@@ -31,7 +32,10 @@ class Reservation extends Component {
                 },
                 {
                     text: 'OK',
-                    onPress: () => this.resetForm(),
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                    },
                 },
             ],
             { cancelable: false },
@@ -48,6 +52,31 @@ class Reservation extends Component {
         });
     }
 
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                }),
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`,
+                },
+                trigger: null,
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
     render() {
         return (
             <ScrollView>
